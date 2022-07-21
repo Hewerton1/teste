ba112d6bee02dc0dd04cc4049d0863d6b95d5e85
Hewerton1 <hewertoncaio14@gmail.com>
2022-07-21 02:17:16 -0300

    <mensagem>

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express';
+
+import bodyParser from 'body-parser';
+
+import films from './controllers/filmController';
+
+const app: Express = express();
+const port: number = 3333;
+
+app.use(bodyParser.json());
+
+app.use('/films', films);
+
+app.get('/', (req:Request, res: Response) => {
+    res.send('Hello world express')
+});
+
+app.listen(port, () => {
+    console.log(`servidor rodando na porta ${port}`)
+});
+
