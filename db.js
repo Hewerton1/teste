213a5660ee822e33ff1288637b9de1e99b23653e
Hewerton Caio <107573852+Hewerton1@users.noreply.github.com>
2022-07-21 02:10:11 -0300

    Delete db.js

--- db.js
+++
@@ -1,5 +0,0 @@
-JavaScript
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('crud', 'root', 'hewertonc', {dialect: 'mysql', host: 'localhost'});
-
-module.exports = sequelize;
